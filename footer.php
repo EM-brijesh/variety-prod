b7e36edcff8db00dced5c2063736501fff81014b
Brijesh-09 <brijeshkori22@gmail.com>
2025-09-18 19:07:58 +0530

    load changes

--- footer.php
+++ footer.php
@@ -51,10 +51,10 @@
 							Connect <span class="toggle-btn"><img src="data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20width%3D%2710.121%27%20height%3D%275.811%27%20viewBox%3D%270%200%2010.121%205.811%27%3E%3Cg%20id%3D%27Chevron_thin%27%20data-name%3D%27Chevron%20thin%27%20transform%3D%27translate%289.061%201.061%29%20rotate%2890%29%27%3E%3Cpath%20id%3D%27Path_21%27%20data-name%3D%27Path%2021%27%20d%3D%27M-4091.213,6256.242l4,4-4,4%27%20transform%3D%27translate%284091.213%20-6256.242%29%27%20fill%3D%27none%27%20stroke%3D%27%23000%27%20stroke-linecap%3D%27round%27%20stroke-linejoin%3D%27round%27%20stroke-width%3D%271.5%27%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E" width="15" height="15"></span>
 						</h5>
 						<div class="links">
-							<a href="#" target="_blank">Instagram</a>
-							<a href="#" target="_blank">Twitter</a>
+							<a href="#" target="_blank">Instagrammmm</a>
+							<a href="#" target="_blank">Twittesafsafasfr</a>
 							<a href="#" target="_blank">YouTube</a>
-							<a href="#" target="_blank">Facebook</a>
+							<a href="#" target="_blank">Facebasdfasfook</a>
 							<a href="#" target="_blank">LinkedIn</a>
 						</div>
 					</div>
@@ -64,7 +64,7 @@
 						<div class="logo_footer">
 							<a href="<?php echo get_permalink(get_page_by_path('home')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/variety-india-logo.webp"
 									class="img-fluid" alt="Logo"></a>
-							<h4>The Business of Entertainment</h4>
+							<h4>test the The Business of Entertainment</h4>
 							<a href="<?php echo get_permalink(get_page_by_path('news-tips')); ?>" class="news-tip-footer">a News Tip? Let us know</a>
 						</div>
 					</div>
